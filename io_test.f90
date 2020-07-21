8f4ecc8fc2619cf35292c7825c8ea95f012aa143
aQuarkyName2 <aQuarkyName2@gmail.com>
2020-07-21 10:11:07 +0100

    added pointer testing

--- io_test.f90
+++ io_test.f90
@@ -2,9 +2,13 @@ program fortran_io_tut
   implicit none
   real, parameter :: PI = 3.14
   logical :: is_pi = .false.
-  real :: myNum = 0.0
+  real :: myNum = 3.141
+  
+  integer, pointer :: p1
+  integer, target :: t1
+
   print *, "Enter number: "
-  read *, myNum
+  !  read *, myNum
 
   if (myNum == PI) then
      print *, "Is pi!"
@@ -12,5 +16,16 @@ program fortran_io_tut
      print *, "Not pi."
   end if
 
+  ! add pointer testing within feature1 branch
+  p1 => t1
+  t1 = 1
+  
+  print *, "p1 == ", p1
+  print *, "t1 == ", t1
+  
+  t1 = 100
+  print *, "p1 == ", p1
+  print *, "t1 == ", t1
+  
   
 end program fortran_io_tut
